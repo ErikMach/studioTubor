cee50d36c23ce9fe2fbcec257d2082964ed18b83
erikmach <e.vf.w00@gmail.com>
2022-10-31 03:58:30 +1100

    final changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 const scheduleCont = document.getElementsByClassName("schedule")[0];
+const selectMenu = document.getElementById("scheduleSelect");
+
+selectMenu.value = localStorage.getItem("selectMenuValue") || "teacher";
+
+const renderSchedule = () => {
+  switch (selectMenu.value) {
+    case "teacher":
+      createScheduleTeacher();
+      break;
+    case "day":
+      createScheduleDay();
+      break;
+  }
+  localStorage.setItem("selectMenuValue", selectMenu.value);
+};
+
+selectMenu.addEventListener("input", renderSchedule);
 
 const c = (tag, attrs = {}, children = []) => {
   const elem = document.createElement(tag);
@@ -66,15 +83,63 @@ const writeTimeRange = (timeRange) => {
   return `${timeConvert(timeRange.start)} - ${timeConvert(timeRange.end)}`;
 };
 
+const createScheduleDay = () => {
+  const table = c("table", {class: "schedule"});
+
+  // Create Schedule by Days
+  const scheduleDay = {};
+  days.forEach(day => {
+    scheduleDay[day] = [];
+  });
+  for (const [teacher, classes] of Object.entries(schedule)) {
+    classes.forEach(classe => {
+      classe.teacher = teacher;
+      scheduleDay[days[classe.day]].push(classe);
+    });
+  }
+
+  const daysOrder = days.slice(1);
+  daysOrder.push("Sunday");
+  daysOrder.forEach(day => {
+    const titleRow = c("tr", {class: "newSet"}, [
+      c("th", {class: "teacher", colspan: 4, textContent: day + "s"})
+    ]);
+    table.appendChild(titleRow);
+
+    scheduleDay[day].forEach((classe, i) => {
+      if (scheduleDay[day][i-1]?.teacher !== classe.teacher || !arrayOfStrEq(scheduleDay[day][i-1]?.dates, classe.dates)) {
+        let datesStr = classe.teacher + "&emsp;<span class='noBold'>(";
+        datesStr += (classe.dates.length === 1) ? `from ${classe.dates[0].split(/\./g).join("/")}` : `${classe.dates[0].split(/\./g).join("/")} to ${classe.dates[classe.dates.length-1].split(/\./g).join("/")}`;
+        datesStr += ")</span>";
+
+        const subTitleRow = c("tr", {}, [
+	  c("th", {class: "day", colspan: 4, innerHTML: datesStr})
+        ]);
+        table.appendChild(subTitleRow);
+      }
+      const infoRow = c("tr", {class: i===scheduleDay[day].length-1 ? "newSet" : ""}, [
+	c("td", {class: "time", textContent: writeTimeRange(classe.time)}),
+	c("td", {class: "studio", innerHTML: `<a href="${studios[classe.studio].mapLink}" target="_blank">${classe.studio}</a>`, title: studios[classe.studio].address}),
+	c("td", {class: "level", textContent: classe.classType}),
+	c("td", {class: "cost", textContent: "$" + classe.cost}),
+      ]);
+      table.appendChild(infoRow);
+    });
+  });
+
+  if (scheduleCont.getElementsByTagName("table")[0]) {
+    scheduleCont.getElementsByTagName("table")[0].replaceWith(table);
+  } else {
+    scheduleCont.appendChild(table);
+  }
+};
 
 const createScheduleTeacher = () => {
   const table = c("table", {class: "schedule"});
 
-  let i = -1;
   for (const [teacher, classes] of Object.entries(schedule)) {
-    i++;
     const titleRow = c("tr", {class: "newSet"}, [
-      c("th", {class: "teacher", colspan: 4, textContent: teacher, style: `background-color:hsl(${i * 0}deg 100% 88%)`})
+      c("th", {class: "teacher", colspan: 4, textContent: teacher})
     ]);
     table.appendChild(titleRow);
 
@@ -99,7 +164,11 @@ const createScheduleTeacher = () => {
     });
   }
 
-  scheduleCont.appendChild(table);
+  if (scheduleCont.getElementsByTagName("table")[0]) {
+    scheduleCont.getElementsByTagName("table")[0].replaceWith(table);
+  } else {
+    scheduleCont.appendChild(table);
+  }  
 };
 
-createScheduleTeacher();
+renderSchedule();
