721147438f71741aca01355476dba2a721bba262
erikmach <e.vf.w00@gmail.com>
2022-10-31 08:40:13 +1100

    all

---
+++ sw.js
@@ -0,0 +1,24 @@
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      "/index.html",
+      "/css/index.css",
+      "/js/index.js",
+      "/js/classSchedule.js",
+      "/js/initSW.js",
+      "/res/img/bg/ballet_1.webp",
+      "/res/img/bg/polunin_2.webp",
+      "./res/fonts/barkelia/Barkelia.ttf",
+      "./res/fonts/lato/Lato.woff2",
+      "./res/fonts/cafeta/Cafeta.woff2",
+      "./res/fonts/english/English.woff2"
+    ])
+  );
+});
+
+self.addEventListener('fetch', function(event) {})
