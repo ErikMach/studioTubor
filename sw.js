d82e44af1b60849a216bfaa62f01541c32a7f837
erikmach <e.vf.w00@gmail.com>
2022-10-31 08:51:49 +1100

    PWA Installation

--- sw.js
+++ sw.js
@@ -6,19 +6,22 @@ const addResourcesToCache = async (resources) => {
 self.addEventListener("install", (event) => {
   event.waitUntil(
     addResourcesToCache([
-      "/index.html",
-      "/css/index.css",
-      "/js/index.js",
-      "/js/classSchedule.js",
-      "/js/initSW.js",
-      "/res/img/bg/ballet_1.webp",
-      "/res/img/bg/polunin_2.webp",
-      "./res/fonts/barkelia/Barkelia.ttf",
-      "./res/fonts/lato/Lato.woff2",
-      "./res/fonts/cafeta/Cafeta.woff2",
-      "./res/fonts/english/English.woff2"
+      "index.html",
+      "css/index.css",
+      "js/index.js",
+      "js/classSchedule.js",
+      "js/initSW.js",
+      "res/img/bg/ballet_1.webp",
+      "res/img/bg/polunin_2.webp",
+      "res/fonts/barkelia/Barkelia.ttf",
+      "res/fonts/lato/Lato.woff2",
+      "res/fonts/cafeta/Cafeta.woff2",
+      "res/fonts/english/English.woff2"
     ])
   );
 });
 
-self.addEventListener('fetch', function(event) {})
+self.addEventListener('fetch', event=> {
+    let response = fetch(event.request);
+    event.respondWith(response);
+});
